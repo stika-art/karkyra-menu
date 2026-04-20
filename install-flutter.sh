4ad6833d1c51018dacf63a3286a023c676652d46
stika-art <stika-art@aikol.local>
2026-04-20 18:00:12 +0600

    Simplify build script for Vercel

--- install-flutter.sh
+++ install-flutter.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-# 1. Доверяем директории (фикс для Git)
+# 1. Безопасность Git
 git config --global --add safe.directory "*"
 
-# 2. Клонируем Flutter, если его нет
+# 2. Установка Flutter (если нет в кэше)
 if [ ! -d "flutter" ]; then
   git clone https://github.com/flutter/flutter.git -b stable --depth 1
 fi
 
-# 3. Определяем полный путь к исполняемому файлу
-FLUTTER_BIN="$(pwd)/flutter/bin/flutter"
+# 3. Путь к бинарнику
+FLUTTER="./flutter/bin/flutter"
 
-# 4. Настройка
-$FLUTTER_BIN config --no-analytics
-$FLUTTER_BIN config --enable-web
+# 4. Диагностика и настройка
+$FLUTTER config --no-analytics
+$FLUTTER config --enable-web
+$FLUTTER doctor
 
-# 5. Сборка
-$FLUTTER_BIN pub get
-$FLUTTER_BIN build web --release --web-renderer=canvaskit
+# 5. Сборка БЕЗ лишних флагов
+$FLUTTER pub get
+$FLUTTER build web --release
